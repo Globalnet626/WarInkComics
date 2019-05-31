20a0f1bfdc44dd86f5d63df47a5d9937c74758f1
globalnet626 <miguelslvn@gmail.com>
2019-05-31 08:15:00 -0700

    Added database skeleton + upload 2 ftp prototype

---
+++ Project/admin/upload.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="upload.aspx.cs" Inherits="Project.admin.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <asp:Button Text="Upload to FTP" runat="server" OnClick="FTPUpload" />
+            <hr />
+            <asp:Label ID="lblMessage" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Project/admin/upload.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project.admin
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        /*protected string FTPUpload(object sender, EventArgs e)
+        {
+            //FTP Server URL.
+            string ftp = "ftp://miguelslvn.com/";
+            string http = "http://miguelslvn.com/images/";
+
+            //FTP Folder name. Leave blank if you want to upload to root folder.
+            string ftpFolder = "/";
+
+            byte[] fileBytes = null;
+
+            //Read the FileName and convert it to Byte array.
+            string fileName = Path.GetFileName(FileUpload1.FileName);
+            using (StreamReader fileStream = new StreamReader(FileUpload1.PostedFile.InputStream))
+            {
+                fileBytes = Encoding.UTF8.GetBytes(fileStream.ReadToEnd());
+                fileStream.Close();
+            }
+
+            try
+            {
+                //Create FTP Request.
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftp + ftpFolder + fileName);
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+
+                //Enter FTP Server credentials.
+                request.Credentials = new NetworkCredential("final_cis136_project", "Pr0Gr@mP@SSW)RD");
+                request.ContentLength = fileBytes.Length;
+                request.UsePassive = true;
+                request.UseBinary = true;
+                request.ServicePoint.ConnectionLimit = fileBytes.Length;
+                request.EnableSsl = false;
+
+                using (Stream requestStream = request.GetRequestStream())
+                {
+                    requestStream.Write(fileBytes, 0, fileBytes.Length);
+                    requestStream.Close();
+                }
+
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+                lblMessage.Text += fileName + " uploaded.<br />";
+                response.Close();
+                return http + fileName;
+            }
+            catch (WebException ex)
+            {
+                throw new Exception((ex.Response as FtpWebResponse).StatusDescription);
+            }
+        }*/
+        protected void FTPUpload(object sender, EventArgs e)
+        {
+            //FTP Server URL.
+            string ftp = "ftp://miguelslvn.com/";
+            string http = "http://miguelslvn.com/images/";
+
+            //FTP Folder name. Leave blank if you want to upload to root folder.
+            string ftpFolder = "/";
+
+            byte[] fileBytes = null;
+
+            //Read the FileName and convert it to Byte array.
+            string fileName = Path.GetFileName(FileUpload1.FileName);
+            using (StreamReader fileStream = new StreamReader(FileUpload1.PostedFile.InputStream))
+            {
+                fileBytes = Encoding.UTF8.GetBytes(fileStream.ReadToEnd());
+                fileStream.Close();
+            }
+
+            try
+            {
+                //Create FTP Request.
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftp + ftpFolder + fileName);
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+
+                //Enter FTP Server credentials.
+                request.Credentials = new NetworkCredential("final_cis136_project", "Pr0Gr@mP@SSW)RD");
+                request.ContentLength = fileBytes.Length;
+                request.UsePassive = true;
+                request.UseBinary = true;
+                request.ServicePoint.ConnectionLimit = fileBytes.Length;
+                request.EnableSsl = false;
+
+                using (Stream requestStream = request.GetRequestStream())
+                {
+                    requestStream.Write(fileBytes, 0, fileBytes.Length);
+                    requestStream.Close();
+                }
+
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+                lblMessage.Text += fileName + " uploaded.<br />";
+                response.Close();
+            }
+            catch (WebException ex)
+            {
+                throw new Exception((ex.Response as FtpWebResponse).StatusDescription);
+            }
+        }
+
+    }
+
+}
